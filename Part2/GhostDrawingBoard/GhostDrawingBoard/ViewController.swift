e88403d7b9b24869eea58020325305f13ac77f09
SubbaReddy <subba.bkk@gmail.com>
2023-10-21 21:44:33 +0530

    Technical Ability: Created project

---
+++ Part2/GhostDrawingBoard/GhostDrawingBoard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GhostDrawingBoard
+//
+//  Created by Subba Reddy on 21/10/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
